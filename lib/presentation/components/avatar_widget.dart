432cca39e39be9f363520bd6bdbb178c0d52a2b5
ohsangsoo <tkdtn3264@gmail.com>
2023-08-26 21:01:14 +0900

    나의 스토리 보드 ui 만들기

--- lib/presentation/components/avatar_widget.dart
+++ lib/presentation/components/avatar_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_clone/presentation/components/avatar_widget.dart';
 
 enum AvatarType { storyBoardAvtar, nickNameAvatar, bottomNavAvatar }
 
@@ -12,11 +14,68 @@ class AvatarWidget extends StatelessWidget {
   AvatarWidget({
     Key? key,
     required this.type,
-    this.hasStroy,
     required this.thumbPath,
+    this.hasStroy,
     this.nickName,
     this.size,
   }) : super(key: key);
+
+//그라데이션 있는 위젯
+  Widget gradationWidget(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      padding: const EdgeInsets.all(4),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.purple,
+              Colors.orange,
+            ]),
+        shape: BoxShape.circle,
+      ),
+      child: Container(
+        padding: EdgeInsets.all(4),
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100),
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.21,
+            height: MediaQuery.of(context).size.height * 0.1,
+            child: CachedNetworkImage(
+              imageUrl: thumbPath,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+Widget MyStoryBoard(BuildContext context){
+    return Container(
+      padding: EdgeInsets.all(4),
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(100),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.21,
+          height: MediaQuery.of(context).size.height * 0.1,
+          child: CachedNetworkImage(
+            imageUrl: thumbPath,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+}
 
   @override
   Widget build(BuildContext context) {
@@ -25,29 +84,11 @@ class AvatarWidget extends StatelessWidget {
         return gradationWidget(context);
         break;
       case AvatarType.nickNameAvatar:
+        return MyStoryBoard(context);
         break;
       case AvatarType.bottomNavAvatar:
+        return Container();
         break;
     }
-    return Container();
   }
-}
-
-//그라데이션 있는 위젯
-Widget gradationWidget(BuildContext context) {
-  return Container(
-    width: MediaQuery.of(context).size.width * 0.16,
-    height: MediaQuery.of(context).size.height * 0.1,
-    margin: const EdgeInsets.symmetric(horizontal: 5),
-    decoration: BoxDecoration(
-      gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [
-            Colors.purple,
-            Colors.orange,
-          ]),
-      shape: BoxShape.circle,
-    ),
-  );
 }
