fc1509e6e0a544f1a75ad5140d02d647e5ee50c8
ohsangsoo <tkdtn3264@gmail.com>
2023-09-04 19:53:06 +0900

    활동  페이지 이동하는 네비게이터 푸시 추가

--- lib/presentation/instagram/pages/instagram_home.dart
+++ lib/presentation/instagram/pages/instagram_home.dart
@@ -1,26 +1,25 @@
-import 'package:flutter/material.dart';
+            import 'package:flutter/material.dart';
 import 'package:flutter_clone/presentation/components/avatar_widget.dart';
 import 'package:flutter_clone/presentation/components/image_data.dart';
 import 'package:flutter_clone/presentation/components/post_widget.dart';
+import 'package:flutter_clone/presentation/instagram/pages/instagram_Reels.dart';
+import 'package:flutter_clone/presentation/instagram/pages/instagram_active_History.dart';
 
-class home extends StatelessWidget {
+class Home extends StatelessWidget {
   //const home({Key? key}) : super(key: key);
-
-
   List<String> storyBoardList = [
     "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
     "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
     "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
     "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
-    "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
-    "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
-
-  ];
-
+    'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/363233336_296568409535930_6371283386676776730_n.jpg?stp=dst-jpg_s160x160&_nc_cat=102&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=PXXzV5bVk3kAX-rj80c&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=00_AfCv0IeSXxyaTQ_33nn_EjQkHhBk6unrnvY5EJdDXIzxWg&oe=64F35F62',
+    'https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/363233336_296568409535930_6371283386676776730_n.jpg?stp=dst-jpg_s160x160&_nc_cat=102&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=PXXzV5bVk3kAX-rj80c&_nc_ht=scontent-ssn1-1.cdninstagram.com&oh=00_AfCv0IeSXxyaTQ_33nn_EjQkHhBk6unrnvY5EJdDXIzxWg&oe=64F35F62',
+];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leadingWidth: 0,
         elevation: 0,
         title: ImageData(
           IconsPath.logo,
@@ -28,7 +27,10 @@ class home extends StatelessWidget {
         ),
         actions: [
           GestureDetector(
-            onTap: () {},
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const ActiveHistory()));
+            },
             child: Padding(
               padding: const EdgeInsets.all(15.0),
               child: ImageData(IconsPath.activeOff,
