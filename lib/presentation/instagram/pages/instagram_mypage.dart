7e89f83396c89054db23e4caf82b435e2984f864
ohsangsoo <tkdtn3264@gmail.com>
2023-09-08 13:53:57 +0900

    마이페이지 infomation 위젯 완성

--- lib/presentation/instagram/pages/instagram_mypage.dart
+++ lib/presentation/instagram/pages/instagram_mypage.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_clone/presentation/components/avatar_widget.dart';
 import 'package:flutter_clone/presentation/components/image_data.dart';
 
-class MyPage extends StatelessWidget {
+class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
 
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  // 인포메이션 안에 텍스트
   Widget _statisticeone(String title, int value) {
     return Column(
       children: [
         Text(
           value.toString(),
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             color: Colors.black,
@@ -18,7 +24,7 @@ class MyPage extends StatelessWidget {
         ),
         Text(
           title,
-          style: TextStyle(
+          style: const TextStyle(
             fontSize: 15,
             color: Colors.black,
           ),
@@ -27,6 +33,7 @@ class MyPage extends StatelessWidget {
     );
   }
 
+  // 인포메이션
   Widget _infomation(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15.0),
@@ -39,14 +46,11 @@ class MyPage extends StatelessWidget {
               AvatarWidget(
                 type: AvatarType.storyBoardAvtar,
                 thumbPath:
-                "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
+                    "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
                 size: 80,
               ),
               SizedBox(
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width * 0.02,
+                width: MediaQuery.of(context).size.width * 0.02,
               ),
               Expanded(
                 child: Row(
@@ -60,16 +64,85 @@ class MyPage extends StatelessWidget {
               ),
             ],
           ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+          const Text(
+            '안녕하세요 oss0302',
+            style: TextStyle(
+              fontSize: 13,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-        SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-        const Text(
-          '안녕하세요 oss0302',
-          style: TextStyle(
-            fontSize: 13,
-            color: Colors.black,
+// _menu()
+  Widget _menu(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  border: Border.all(
+                    color: const Color(0xffdedede),
+                  ),
+                ),
+                child: const Text(
+                  '프로필 편집',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
           ),
-        )
-      ],
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3),
+                  border: Border.all(
+                    color: const Color(0xffdedede),
+                  ),
+                ),
+                child: const Text(
+                  '프로필 공유',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width * 0.01,),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3),
+                border: Border.all(
+                  color: const Color(0xffdedede),
+                ),
+                color: const Color(0xffefefef),
+              ),
+              child: ImageData(IconsPath.addFriend)
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -118,6 +191,7 @@ class MyPage extends StatelessWidget {
         child: Column(
           children: [
             _infomation(context),
+            _menu(context),
           ],
         ),
       ),
