7ba26a6a5207db47cafb011f25704c88278ce26d
ohsangsoo <tkdtn3264@gmail.com>
2023-09-08 13:14:48 +0900

    마이페이지 infomation 위젯 완성

--- lib/presentation/instagram/pages/instagram_mypage.dart
+++ lib/presentation/instagram/pages/instagram_mypage.dart
@@ -28,36 +28,49 @@ class MyPage extends StatelessWidget {
   }
 
   Widget _infomation(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0),
-          child: Row(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               AvatarWidget(
                 type: AvatarType.storyBoardAvtar,
                 thumbPath:
-                    "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
+                "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/280714442_973301870035556_6298617763728239387_n.jpg?stp=dst-jpg_s160x160&_nc_cat=109&ccb=1-7&_nc_sid=8ae9d6&_nc_ohc=o2fTvbHP6NAAX-5kcaz&_nc_ht=scontent-gmp1-1.cdninstagram.com&oh=00_AfD0vsDKrnuGc_SKLSdl8qHPSfGqYo7cwA0vm-hTihp48A&oe=64ED7E35",
                 size: 80,
               ),
               SizedBox(
-                width: MediaQuery.of(context).size.width * 0.02,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width * 0.02,
               ),
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Expanded(child: _statisticeone('Post', 15)),
-                    Expanded(child:_statisticeone('Followers', 11)),
-                    Expanded(child:_statisticeone('Following', 4)),
+                    Expanded(child: _statisticeone('Followers', 11)),
+                    Expanded(child: _statisticeone('Following', 4)),
                   ],
                 ),
               ),
             ],
           ),
-        ),
+
+        SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+        const Text(
+          '안녕하세요 oss0302',
+          style: TextStyle(
+            fontSize: 13,
+            color: Colors.black,
+          ),
+        )
       ],
+      ),
     );
   }
 
